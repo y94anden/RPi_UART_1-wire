f9b034993699eeda3952d3d58a8e40724bcc5393
Anders Englund <y94anden@hotmail.com>
2013-08-20 00:49:58 +0200

    Added GPL v2 headers

--- onewire.py
+++ onewire.py
@@ -1,6 +1,26 @@
 #!/usr/bin/python3
 # -*- Coding: utf-8 -*-
 
+"""
+onewire.py A 1-wire implementation using the UART in the Raspberry PI
+Copyright (C) 2013 Anders Englund
+
+This file is part of RPi_UART_1-wire.
+
+RPi_UART_1-wire is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+RPi_UART_1-wire is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with RPi_UART_1-wire.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 import serial
 import logging
 from datetime import datetime
--- simulator.py
+++ simulator.py
@@ -1,4 +1,24 @@
 """
+simulator.py: A simulator of the UART and 1-wire devices for use in testcases.
+Copyright (C) 2013 Anders Englund
+
+This file is part of RPi_UART_1-wire.
+
+RPi_UART_1-wire is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+RPi_UART_1-wire is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with RPi_UART_1-wire.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+"""
 This module simulates an uart in case there is no real UART.
 
 There is also a class that can simulate a 1-wire device.
